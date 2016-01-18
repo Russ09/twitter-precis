f89a30b424d7dc5a6ebaa9aa470f03de93e12a0d
Russ09 <russjbates@gmail.com>
2016-01-18 20:11:36 +0000

    -
    -

--- tweetHandler.cpp
+++ tweetHandler.cpp
@@ -10,12 +10,15 @@ private:
 
 
 public:
+
+	void		tweetHandler(); //Constructor
+	void		~tweetHandler(); //Destructor
+
 	void		setConsumerKey(char *consumerKey);
 	void		setConsumerSecret(char *consumerSecret);
 	void		setLoginDetails(char *username, char *password);
 	void		authorizeAccess(void);
 
 	Tweet		getTweet();
-
-
+	Tweet*		getTweetsGivenHashtag(char *hashtag);
 }
